2c1fd2d849adc1adaa38fe1d92625bed6c4581ed
wjh <wjh>
2016-05-05 20:44:36 +0800

    AS个人配置

---
+++ config/fileTemplates/includes/File Header.java
@@ -0,0 +1,13 @@
+
+/*
+ * @创建者     wjh
+ * @创建时间   ${DATE} ${TIME}
+ * @描述	      ${TODO}
+ *
+ * @项目名称:  $PROJECT_NAME$
+ * @包名       $PACKAGE_NAME$
+ * @版本       $Revision$
+ * @更新者     $Author$
+ * @更新时间   $Date$
+ * @更新描述   ${TODO}
+ */
